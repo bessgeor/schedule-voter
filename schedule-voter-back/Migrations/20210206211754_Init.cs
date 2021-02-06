51e8c6ba4d7cb03cfc765428e8e7febf8b4ad21d
bessgeor <bessgeor@gmail.com>
2021-02-07 00:20:46 +0300

    Add DB infra

---
+++ schedule-voter-back/Migrations/20210206211754_Init.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace schedule_voter_back.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "schedule_voter");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "schedule_voter",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    static_name = table.Column<string>(nullable: false),
+                    gw2_account = table.Column<string>(nullable: false),
+                    dis_account = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vote_results",
+                schema: "schedule_voter",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(nullable: false),
+                    tourney = table.Column<DateTime>(nullable: false),
+                    vote = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vote_results", x => new { x.user_id, x.tourney });
+                    table.ForeignKey(
+                        name: "FK_vote_results_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "schedule_voter",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_static_name_dis_account_gw2_account",
+                schema: "schedule_voter",
+                table: "users",
+                columns: new[] { "static_name", "dis_account", "gw2_account" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vote_results",
+                schema: "schedule_voter");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "schedule_voter");
+        }
+    }
+}
---
+++ schedule-voter-back/_Db/Db.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace schedule_voter_back._Db
+{
+	class Db : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<VoteResult> VoteResults { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? "Host=localhost;Port=5432;Database=schedule_voter_db;Username=schedule_voter_db;Password=AsdasafASDasdasdASDsa2312@#!qd1@#@WS");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.HasDefaultSchema("schedule_voter");
+			modelBuilder.Entity<User>(users =>
+			{
+				users.ToTable("users").HasKey(x => x.Id);
+
+				users.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd().UseNpgsqlSerialColumn();
+				users.Property(x => x.StaticName).HasColumnName("static_name").IsRequired();
+				users.Property(x => x.DisAccount).HasColumnName("dis_account").IsRequired();
+				users.Property(x => x.Gw2Account).HasColumnName("gw2_account").IsRequired();
+
+				users.HasIndex(x => new { x.StaticName, x.DisAccount, x.Gw2Account }).IsUnique();
+
+				users.HasMany(x => x.Votes).WithOne(x => x.User).HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Restrict);
+			});
+			modelBuilder.Entity<VoteResult>(vr =>
+			{
+				vr.ToTable("vote_results").HasKey(x => new { x.UserId, x.Tourney });
+
+				vr.Property(x => x.UserId).HasColumnName("user_id");
+				vr.Property(x => x.Tourney).HasColumnName("tourney");
+				vr.Property(x => x.Vote).HasColumnName("vote");
+			});
+		}
+	}
+}
